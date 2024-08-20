3d40dc7bea7b4b85f634306fa50089c24c7a4f8a
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-20 13:16:51 -0300

    Add exercise 'Lista de Tarefas'

---
+++ src/collections/list/lista_tarefas/ListaTarefas.java
@@ -0,0 +1,51 @@
+package collections.list.lista_tarefas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    private List<Tarefa> tarefas;
+
+    public ListaTarefas() {
+        this.tarefas = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        this.tarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        for(Tarefa tarefa : tarefas) {
+            if(tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                this.tarefas.remove(tarefa);
+                break;
+            }
+        }
+    }
+
+    public void removerTarefaRepetida(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+
+        for(Tarefa tarefa : tarefas) {
+            if(tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+
+        tarefas.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return this.tarefas.size();
+    }
+
+    public List<String> obterDescricoesTarefas() {
+        List<String> descricoes = new ArrayList<>();
+
+        for(Tarefa tarefa : tarefas) {
+            descricoes.add(tarefa.getDescricao());
+        }
+
+        return descricoes;
+    }
+}
---
+++ src/collections/list/lista_tarefas/Main.java
@@ -0,0 +1,33 @@
+package collections.list.lista_tarefas;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        // Adiciona itens
+        listaTarefas.adicionarTarefa("Limpar a casa");
+        listaTarefas.adicionarTarefa("Alimentar o gato");
+        listaTarefas.adicionarTarefa("Comprar verduras");
+
+        System.out.println(listaTarefas.obterDescricoesTarefas());
+
+        // Imprime o tamanho
+        System.out.println(listaTarefas.obterNumeroTotalTarefas());
+
+        // Deleta item
+        listaTarefas.removerTarefa("Comprar verduras");
+
+        System.out.println(listaTarefas.obterDescricoesTarefas());
+
+        // Deleta itens
+        listaTarefas.adicionarTarefa("Comprar verduras");
+        listaTarefas.adicionarTarefa("Comprar verduras");
+        listaTarefas.adicionarTarefa("Comprar verduras");
+
+        System.out.println(listaTarefas.obterDescricoesTarefas());
+
+        listaTarefas.removerTarefaRepetida("Comprar verduras");
+
+        System.out.println(listaTarefas.obterDescricoesTarefas());
+    }
+}
