a893a04c0304379e728e7bdc52a3a10373901c94
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-20 13:51:24 -0300

    Add exercise 'Carrinho de Compras'

---
+++ src/collections/list/lista_de_compras/CarrinhoDeCompras.java
@@ -0,0 +1,42 @@
+package collections.list.lista_de_compras;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> listaItens;
+
+    public CarrinhoDeCompras() {
+        this.listaItens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        this.listaItens.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome) {
+        List<Item> listaDeRemocao = new ArrayList<>();
+
+        for(Item item : listaItens) {
+            if(item.getNome().equalsIgnoreCase(nome)) {
+                listaDeRemocao.add(item);
+            }
+        }
+
+        listaItens.removeAll(listaDeRemocao);
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0;
+
+        for(Item item : listaItens) {
+            valorTotal += item.getPreco() * item.getQuantidade();
+        }
+
+        return valorTotal;
+    }
+
+    public void exibirItens() {
+        System.out.println(listaItens);
+    }
+}
---
+++ src/collections/list/lista_de_compras/Main.java
@@ -0,0 +1,24 @@
+package collections.list.lista_de_compras;
+
+public class Main {
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoCompras = new CarrinhoDeCompras();
+
+        // Adição de itens
+        carrinhoCompras.adicionarItem("Teclado", 29.90, 3);
+        carrinhoCompras.adicionarItem("Mouse", 19.90, 2);
+        carrinhoCompras.adicionarItem("Caixa de som", 49.90, 1);
+
+        carrinhoCompras.exibirItens();
+
+        // Exclusão de itens
+        carrinhoCompras.removerItem("teclado");
+
+        carrinhoCompras.exibirItens();
+
+        // Calcular valor total
+        System.out.println("Valor total: R$"
+                + String.format("%.2f", carrinhoCompras.calcularValorTotal())
+        );
+    }
+}
