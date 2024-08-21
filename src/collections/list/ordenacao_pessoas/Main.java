f11f471108641cbec26b91c54827e7db82d82111
Gabriel <gabriel.gracilianomb@gmail.com>
2024-08-21 14:25:38 -0300

    Add exercise 'Ordenação de Pessoas'

---
+++ src/collections/list/ordenacao_pessoas/Main.java
@@ -0,0 +1,15 @@
+package collections.list.ordenacao_pessoas;
+
+public class Main {
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.36);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
